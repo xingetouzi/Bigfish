4f81253b748f35fcc5cab44dfae13f3b7bbecb23
BurdenBear <541795600@qq.com>
2016-01-09 11:52:48 +0800

    1

--- Bigfish/utils/ligerUI_util.py
+++ Bigfish/utils/ligerUI_util.py
@@ -38,7 +38,7 @@ class LigerUITranslator:
         columns = [self._get_column_dict(dataframe.index.name)] + list(
                 map(lambda x: self._get_column_dict(x), dataframe.columns))
         temp = dataframe.applymap(lambda x: round(x, self.__precision)).fillna('/')
-        temp = pd.concat([temp, pd.DataFrame(temp.index)], axis=1, ignore_index=True)
+        temp[temp.index.name] = temp.index.to_serires().astype(str)
         data = {'Rows': temp.to_dict('records')}
         return dict(columns=columns, data=data, **self.__options)
 

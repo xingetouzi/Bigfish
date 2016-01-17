32826ee35e01ec78f44bcb2740ea3a827e13260d
BurdenBear <541795600@qq.com>
2016-01-17 23:52:00 +0800

    1

--- bigfish_functions/ATR.py
+++ bigfish_functions/ATR.py
@@ -5,8 +5,7 @@ def ATR(length=10):
     if barnum == 1:
         atr[0] = true_range(0)
     elif barnum <= length:
-        for i in range(barnum - 1, -1, -1):
-            atr[0] = atr[1] + true_range(i)
+        atr[0] = atr[1] + true_range(i)
     else:
         atr[0] = atr[1] + true_range(0) - true_range(length - 1)
     return atr[0] / min(length, barnum)

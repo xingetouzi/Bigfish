91b0a65ed9dcb64717e31fedc982c94964245e65
BurdenBear <541795600@qq.com>
2016-02-20 17:10:47 +0800

    修复summation中的bug，提交了正确的summation系统函数代码

--- bigfish_functions/summation.py
+++ bigfish_functions/summation.py
@@ -1,12 +1,13 @@
 # -*- coding:utf-8 -*-
 
 def summation(length,price=None):
-    export(Sum)
+    export(sum_)
     if not price:
         price=close
-    if barnum <= length:
-        for i in range(length):
-            Sum[0]=Sum[0]+price[i]
+    if barnum == 1:
+        sum_[0] = price[0]
+    elif barnum <= length:
+            sum_[0]=sum_[1]+price[0]
     else:
-        Sum[0]=Sum[1]+price[0]-price[length]
-    return Sum[0]
+        sum_[0]=sum_[1]+price[0]-price[length]
+    return sum_[0]

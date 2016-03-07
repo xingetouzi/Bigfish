64f4811b3023c8c18324c2ca955fd4ff07270848
BurdenBear <541795600@qq.com>
2016-03-07 16:53:30 +0800

    现在symbol list 从配置文件中获取，限制了交叉盘

--- Bigfish/config.py
+++ Bigfish/config.py
@@ -3,9 +3,31 @@
 """
 项目配置文件
 """
-__all__ = ['MEMORY_DEBUG', 'DATABASE', 'ASYNC', 'MODULES_IMPORT']
+__all__ = ["MEMORY_DEBUG", "DATABASE", "ASYNC", "MODULES_IMPORT", "SYMBOL_LIST", "CROSS_TRADE", "SYMBOL_LIST"]
 
-MEMORY_DEBUG = False  # 是否开启内存检测模式
-DATABASE = 'mysql'
+MEMORY_DEBUG = True  # 是否开启内存检测模式
+DATABASE = "mysql"  # 数据库
 ASYNC = False  # 数据拉取方式
-MODULES_IMPORT = ['time', 'datetime']  # 允许在策略代码中导入的模块列表
+MODULES_IMPORT = ["time", "datetime"]  # 允许在策略代码中导入的模块列表
+CROSS_TRADE = False  # 是否开启交叉盘
+SYMBOL_LIST = {"EUR/USD": {"en_name": "EUR/USD", "zh_name": "欧元/美元"},
+               "XAU/USD": {"en_name": "XAU/USD", "zh_name": "黄金/美元"},
+               "GBP/USD": {"en_name": "GBP/USD", "zh_name": "英镑/美元"},
+               "USD/JPY": {"en_name": "USD/JPY", "zh_name": "美元/日元"},
+               "AUD/USD": {"en_name": "AUD/USD", "zh_name": "澳元/美元"},
+               "USD/CAD": {"en_name": "USD/CAD", "zh_name": "美元/加元"},
+               "USD/CHF": {"en_name": "USD/CHF", "zh_name": "美元/瑞郎"},
+               }
+if CROSS_TRADE:
+    SYMBOL_LIST.update({
+        "EUR/JPY": {"en_name": "EUR/JPY", "zh_name": "欧元/日元"},
+        "GBP/JPY": {"en_name": "GBP/JPY", "zh_name": "英镑/日元"},
+        "AUD/JPY": {"en_name": "AUD/JPY", "zh_name": "澳元/日元"},
+        "EUR/CAD": {"en_name": "EUR/CAD", "zh_name": "欧元/加元"},
+        "EUR/GBP": {"en_name": "EUR/GBP", "zh_name": "欧元/英镑"},
+        "EUR/AUD": {"en_name": "EUR/AUD", "zh_name": "欧元/澳元"},
+        "EUR/CHF": {"en_name": "EUR/CHF", "zh_name": "欧元/瑞郎"},
+        "GBP/CHF": {"en_name": "GBP/CHF", "zh_name": "英镑/瑞郎"},
+        "AUD/NZD": {"en_name": "AUD/NZD", "zh_name": "澳元/纽元"},
+        "EUR/NZD": {"en_name": "EUR/NZD", "zh_name": "欧元/纽元"},
+    })
--- Bigfish/store/symbol_manage.py
+++ Bigfish/store/symbol_manage.py
@@ -1,81 +1,13 @@
 # -*- coding: utf-8 -*-
 
-import sqlite3
 from Bigfish.models import Symbol
-import os
+from Bigfish.config import SYMBOL_LIST
+__all__ = ['get_all_symbols']
 
-
-DATABASE_NAME = "BigfishCode/strategy.db"  # 这里也是硬编码，应该写入配置文件中
-SYMBOL_LIST = {"EUR/USD": "欧元/美元",
-               "XAU/USD": "黄金/美元",
-               "GBP/USD": "英镑/美元",
-               "EUR/JPY": "欧元/日元",
-               "USD/JPY": "美元/日元",
-               "GBP/JPY": "英镑/日元",
-               "AUD/JPY": "澳元/日元",
-               "EUR/CAD": "欧元/加元",
-               "EUR/GBP": "欧元/英镑",
-               "AUD/USD": "澳元/美元",
-               "EUR/AUD": "欧元/澳元",
-               "USD/CAD": "美元/加元",
-               "USD/CHF": "美元/瑞郎",
-               "EUR/CHF": "欧元/瑞郎",
-               "GBP/CHF": "英镑/瑞郎",
-               "AUD/NZD": "澳元/纽元",
-               "EUR/NZD": "欧元/纽元",
-               }
-
-
-def connect():
-    conn = sqlite3.connect(os.path.join(os.path.expanduser("~"), DATABASE_NAME))
-    return conn
-
-
-def create_symbol():
-    """
-    创建交易品种数据库表
-    """
-    # TODO (添加)资产的其他信息，如滑点、手续费等信息
-    create_sql = """
-        create table if not exists category (
-            id integer primary key autoincrement,
-            en_name varchar(10),
-            zh_name varchar(20))
-        """
-    with connect() as conn:
-        conn.execute(create_sql)
-        conn.commit()
-
-
-def init_symbol():
-    # TODO 扩充品种, 修改等,或者新添修改表结构的函数
-    with connect() as conn:
-        conn.execute("DELETE FROM category")
-        sql_list = [(en_name, zh_name) for en_name, zh_name in SYMBOL_LIST.items()]
-        conn.executemany("INSERT INTO category(en_name, zh_name) values(?, ?)", sql_list)
-        conn.commit()
-
-
-def exists():
-    exist_sql = "SELECT COUNT(*) FROM sqlite_master where type='table' and name='category'"
-    with connect() as conn:
-        cursor = conn.execute(exist_sql)
-        ret = cursor.fetchone() is not None
-        return ret
+_all_symbols = [Symbol(s['en_name'].replace('/', ''), s['zh_name']) for s in SYMBOL_LIST.values()]
 
 
 # ================下面是对模块外暴露的方法================ #
 def get_all_symbols():
-    all_symbol = []
-    get_all_sql = "SELECT en_name, zh_name FROM category"
-    with connect() as conn:
-        cursor = conn.execute(get_all_sql)
-        for row in cursor.fetchall():
-            category = Symbol(row[0].replace('/', ''), row[1])
-            all_symbol.append(category)
-    return all_symbol
-
-
-if __name__ == '__main__':
-    create_symbol()
-    init_symbol()
+    global _all_symbols
+    return _all_symbols.copy()

10d13b45c3f94cfa49a902ef601119fb97604ad4
simafei <1609048292@qq.com>
2016-04-07 10:50:40 +0800

    添加了web模块

--- Bigfish/web/views.py
+++ Bigfish/web/views.py
@@ -1,47 +1,10 @@
 import tornado.web
-from Bigfish.web.session import Session
-
-
-def login_required(f):
-    def _wrapper(self, *args, **kwargs):
-        print(self.get_current_user())
-        logged = self.get_current_user()
-        if logged is None:
-            self.write('no login')
-            self.finish()
-        else:
-            ret = f(self,*args, **kwargs)
-    return _wrapper
 
 
 class BaseHandler(tornado.web.RequestHandler):
 
     def __init__(self, *argc, **argkw):
         super(BaseHandler, self).__init__(*argc, **argkw)
-        self.session = Session(self.application.session_manager, self)
 
-    def get_current_user(self):
-        return self.session.get("user_name")
-
-
-class MainHandler(BaseHandler):
-
-    @login_required
-    def get(self):
-        username = self.get_current_user()
-        # print('start..')
-        # print(username)
-        # print(self.session['nima'])
-        if username is None:
-            self.write('nima')
-        else:
-            self.write("What's up, " + username + "?")
-
-
-class LoginHandler(BaseHandler):
-    def get(self):
-        # print(self.get_argument("name"))
-        self.session["user_name"] = self.get_argument("name")
-        self.session["nima"] = 'xiaorui.cc'
-        self.session.save()
-        self.write('你的session已经欧了')
+    def post(self):
+        self.write("")

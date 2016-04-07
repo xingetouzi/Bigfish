dfb1e9eb0a1edbee29ca541000e07fab37313ab7
simafei <1609048292@qq.com>
2016-04-07 11:18:09 +0800

    ..

--- Bigfish/web/views.py
+++ Bigfish/web/views.py
@@ -7,4 +7,7 @@ class BaseHandler(tornado.web.RequestHandler):
         super(BaseHandler, self).__init__(*argc, **argkw)
 
     def post(self):
-        self.write("result")
+        print(self.get_arguments("name"))
+        print(self.get_body_arguments("name"))
+        # self.write("json")
+        self.finish()

f9fb82e10c36fa906f79b7b9b11d4e4dbbf68ed0
BurdenBear <541795600@qq.com>
2016-05-13 11:09:53 +0800

    runtime_script 模块级别的日志

--- Bigfish/app/runtime_script.py
+++ Bigfish/app/runtime_script.py
@@ -1,17 +1,18 @@
+import sys
+import codecs
+import ujson as json
+import logging
+import os
+import traceback
+from logging.handlers import RotatingFileHandler
 from Bigfish.app.runtime_singal import RuntimeSignal
 from Bigfish.models.model import User
 from Bigfish.store.directory import UserDirectory
 
-
 DEBUG = False
 
-if __name__ == '__main__':
-    import sys
-    import codecs
-    import ujson as json
-    import logging
-    import os
 
+def main():
     if DEBUG:
         file_name = "testcode9.py"
         config = dict(
@@ -24,7 +25,6 @@ if __name__ == '__main__':
         )
         file = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'test', file_name)
 
-
         def set_handle(logger):
             console = logging.StreamHandler(stream=sys.stdout)
             console.setLevel(logging.DEBUG)
@@ -36,10 +36,6 @@ if __name__ == '__main__':
     else:
         config = json.loads(sys.argv[1])
         file = config.pop('file', '')
-        log_dir = config.pop('log_dir', '/root/BigfishLog')
-
-        from logging.handlers import RotatingFileHandler
-
 
         def set_handle(logger, user=config.get("user", "non-existent user")):
             path = os.path.join(UserDirectory(User(user)).get_temp_dir(), "runtime.log")
@@ -61,3 +57,17 @@ if __name__ == '__main__':
         signal.init()
         set_handle(signal.logger)
         signal.start()
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except:
+        path = os.path.join(UserDirectory.__get_root__(), "runtime.log")
+        logger = logging.getLogger("RuntimeScript")
+        rt_handler = RotatingFileHandler(path, maxBytes=10 * 1024 * 1024, backupCount=5)
+        rt_handler.setLevel(logging.DEBUG)
+        formatter = logging.Formatter('%(asctime)s %(filename)-20s[line:%(lineno)-3d] %(levelname)-8s %(message)s')
+        rt_handler.setFormatter(formatter)
+        logger.addHandler(rt_handler)
+        logger.warning("\n%s" % traceback.format_exc())

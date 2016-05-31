92efb6c9ef69a789c5c12d0b08637c0afab6aad6
BurdenBear <541795600@qq.com>
2016-05-31 11:14:04 +0800

    sharperatio计算的修正

--- Bigfish/app/sharpe_calculator.py
+++ Bigfish/app/sharpe_calculator.py
@@ -9,13 +9,16 @@ class SharpeCalculator:
         self.__pnl.sort(key=lambda x: x["x"])
         self.__pnl_index = list(map(lambda x: x["x"], self.__pnl))
 
-    def get_sharpe(self, start_time, end_time):
+    def get_sharpe(self, capital_base, start_time, end_time):
         st = int(parse(start_time).timestamp())
         et = int(parse(end_time).timestamp())
         si = bisect.bisect_left(self.__pnl_index, st)
         ei = bisect.bisect_right(self.__pnl_index, et)
+        if si >= len(self.__pnl):
+            return 0
         base = self.__pnl[si]["y"]
-        records = [{"x": item["x"], "y": item["y"] - base} for item in self.__pnl[si:ei]]
-        manager = StrategyPerformanceManagerOnline(records, {}, {})
+        records = [{"x": item["x"], "y": (item["y"] - base) / (base + 100)}
+                   for item in self.__pnl[si:ei]]
+        manager = StrategyPerformanceManagerOnline(records, {}, {}, capital_base=base*capital_base)
         performance = manager.get_performance()
         return performance.sharpe_ratio.total

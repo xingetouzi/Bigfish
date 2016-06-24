dc4bb1cb479fd025decd03c74e21d2bbc6586790
BurdenBear <541795600@qq.com>
2016-06-24 15:48:01 +0800

    日志按时间滚动

--- Bigfish/app/runtime_script.py
+++ Bigfish/app/runtime_script.py
@@ -1,47 +1,45 @@
 import sys
-import codecs
 import logging
 import os
 import traceback
-from logging.handlers import RotatingFileHandler
+from datetime import datetime
+from logging.handlers import TimedRotatingFileHandler
+
+log_format = "%(asctime)s %(filename)-20s[line:%(lineno)-4d] %(levelname)-8s %(message)s"
+log_root = os.path.join(os.path.expanduser("~"), "BigfishLog")
+if not os.path.exists(log_root):
+    os.mkdir(log_root)
+
+
+def namer(name):
+    return os.path.join(log_root, os.path.basename(name))
+
+
+def set_handler(logger_name, user, file, level=logging.DEBUG):
+    dir_name = os.path.join(log_root, user)
+    if not os.path.exists(dir_name):
+        os.mkdir(dir_name)
+    path = os.path.join(log_root, user, "%s.log" % file)
+    logger = logging.getLogger(logger_name)
+    rt_handler = TimedRotatingFileHandler(path, when='m', interval=2, backupCount=100, encoding='utf-8')
+    rt_handler.setLevel(level)
+    rt_handler.namer = namer
+    formatter = logging.Formatter(log_format)
+    rt_handler.setFormatter(formatter)
+    logger.addHandler(rt_handler)
 
 
 def main():
-    # code_file = sys.argv[1]
-    # config_file = sys.argv[2]
-    # with codecs.open(code_file, "r", "utf-8") as f:
-    #     code = f.read()
-    #     f.close()
-    # with codecs.open(config_file, "r", "utf-8") as f:
-    #     config = json.loads(f.read())
-    #     f.close()
     userid = sys.argv[1]
-    rd = runtime_data(userid)
-    config = rd.get_config()
-    config['symbols'] = [config['symbols']]
-    code = rd.get_code()
 
-    def set_handle(logger, user=config.get("user", "non-existent user")):
-        path = os.path.join(UserDirectory(User(user)).get_temp_dir(), "runtime.log")
-        rt_handler = RotatingFileHandler(path, maxBytes=10 * 1024 * 1024, backupCount=5)
-        rt_handler.setLevel(logging.DEBUG)
-        formatter = logging.Formatter('%(asctime)s %(filename)-20s[line:%(lineno)-3d] %(levelname)-8s %(message)s')
-        rt_handler.setFormatter(formatter)
-        logger.addHandler(rt_handler)
-
-    if code:
-        if config.get("start_time", None) is not None:
-            signal = TracebackSignal()
-        else:
-            signal = RuntimeSignal()
-        signal.code = code
-        signal.set_config(BfConfig(**config))
-        signal.init()
-        set_handle(signal.logger)
-        signal.start()
-
-
-if __name__ == '__main__':
+    if not userid:
+        name = "Total"
+    else:
+        name = userid
+    set_handler("Script", name, name)
+    if not userid:
+        logging.getLogger("Script").fatal("no userid input!!!")
+        return
     try:
         from Bigfish.store.directory import UserDirectory
         import ujson as json
@@ -50,17 +48,24 @@ if __name__ == '__main__':
         from Bigfish.models.model import User
         from Bigfish.models.config import BfConfig
         from Bigfish.web_utils.runtime_data_man import runtime_data
-        main()
+        rd = runtime_data(userid)
+        config = rd.get_config()
+        config['symbols'] = [config['symbols']]
+        code = rd.get_code()
+        time_start = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        if code:
+            if config.get("start_time", None) is not None:
+                signal = TracebackSignal()
+            else:
+                signal = RuntimeSignal()
+            signal.code = code
+            signal.set_config(BfConfig(**config))
+            signal.init()
+            set_handler(signal.logger.name, userid, userid + '.' + time_start)
+            signal.start()
     except:
-        if len(sys.argv) > 1:
-            user_id = sys.argv[1]
-            path = os.path.join(UserDirectory(User(user_id)).get_temp_dir(), "runtime.log")
-        else:
-            path = os.path.join(UserDirectory.__get_root__(), "runtime.log")
-        logger = logging.getLogger("RuntimeScript")
-        rt_handler = RotatingFileHandler(path, maxBytes=10 * 1024 * 1024, backupCount=5)
-        rt_handler.setLevel(logging.DEBUG)
-        formatter = logging.Formatter('%(asctime)s %(filename)-20s[line:%(lineno)-3d] %(levelname)-8s %(message)s')
-        rt_handler.setFormatter(formatter)
-        logger.addHandler(rt_handler)
-        logger.warning("\n%s" % traceback.format_exc())
+        logging.getLogger("Script").error("%s\n" % traceback.format_exc())
+
+
+if __name__ == '__main__':
+    main()

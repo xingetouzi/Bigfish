085ed8b4598b4ed2bf31b4a525f339c0750ec912
BurdenBear <541795600@qq.com>
2016-07-20 10:01:52 +0800

    Bolling

--- bigfish_functions/Bolling.py
+++ bigfish_functions/Bolling.py
@@ -4,14 +4,15 @@
 # length 时间长度 int
 # price  时间序列数据 序列数组 默认为收盘价数据
 # offset 位移数(从前多少根bar开始) int 默认为0
+# width Bolling带半带宽为width*标准差
 # 返回值:
 # 如果有足够的bar用于计算，返回Boll对象,有以下属性;否则返回None
 # Boll.ma: 移动平均，即bolling中轨线;
-# Boll.std: 1/4 Boll带宽;
+# Boll.std: 标准差;
 # Boll.up: Boll带上轨线;
 # Boll.down: Boll带下轨线;
 
-def Bolling(length, price=None, offset=0):
+def Bolling(length, price=None, offset=0, width=2):
     class Boll:
         __slot__ = ['ma', 'up', 'down', 'std']
 
@@ -25,6 +26,6 @@ def Bolling(length, price=None, offset=0):
         result = Boll()
         result.ma = ma
         result.std = std
-        result.up = ma + 2 * std
-        result.down = ma - 2 * std
+        result.up = ma + width * std
+        result.down = ma - width * std
         return result
